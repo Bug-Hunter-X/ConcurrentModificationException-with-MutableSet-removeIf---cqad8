0a22e11443dea34e360a060aa9150a62cfb3fb31
BugHunter-X <askzenai@gmail.com>
2024-12-14 01:12:07 +0100

    Add bug.kt file

---
+++ bug.kt
@@ -0,0 +1,11 @@
+```kotlin
+fun main() {
+    val list = mutableListOf(1, 2, 3, 4, 5)
+    list.removeIf { it % 2 == 0 }
+    println(list) // Output: [1, 3, 5]
+
+    val set = mutableSetOf(1, 2, 3, 4, 5)
+    set.removeIf { it % 2 == 0 }
+    println(set) //This will throw ConcurrentModificationException
+}
+```
